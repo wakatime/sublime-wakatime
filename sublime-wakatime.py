2357f1325cf7d716ec1eacfe481f10b4bdb2b2f5
Alan Hamlett <alan.hamlett@gmail.com>
2013-07-07 18:38:01 -0700

    version 0.2.1. using new actions api scheme.

--- sublime-wakatime.py
+++ sublime-wakatime.py
@@ -1,10 +1,11 @@
-""" ======================================================
+""" ==========================================================
 File:        sublime-wakatime.py
-Description: Automatic time tracking for Sublime Text 2.
+Description: Automatic time tracking for Sublime Text 2 and 3.
 Maintainer:  Wakati.Me <support@wakatime.com>
-Version:     0.1.0
-======================================================="""
+Website:     https://www.wakati.me/
+==========================================================="""
 
+__version__ = '0.2.1'
 
 import time
 import uuid
@@ -15,52 +16,77 @@ import sublime
 import sublime_plugin
 
 
-# Create logfile if does not exist
-call(['touch', '~/.wakatime.log'])
+# Prompt user if no activity for this many minutes
+AWAY_MINUTES = 10
 
+# globals
 PLUGIN_DIR = dirname(realpath(__file__))
-API_CLIENT = '%s/libs/wakatime.py' % PLUGIN_DIR
-INSTANCE_ID = str(uuid.uuid4())
+API_CLIENT = '%s/packages/wakatime/wakatime.py' % PLUGIN_DIR
+LAST_ACTION = 0
+LAST_FILE = None
 
 
-def get_api_key():
-    api_key = None
-    try:
-        cf = open(expanduser('~/.wakatime'))
-        for line in cf:
-            line = line.split('=', 1)
-            if line[0] == 'api_key':
-                api_key = line[1].strip()
-        cf.close()
-    except IOError:
-        pass
-    return api_key
+def api(targetFile, timestamp, isWrite=False, endtime=None):
+    global LAST_ACTION, LAST_FILE
+    if not targetFile:
+        targetFile = LAST_FILE
+    if targetFile:
+        cmd = ['python', API_CLIENT,
+            '--file', targetFile,
+            '--time', str('%f' % timestamp),
+            '--plugin', 'sublime-wakatime/%s' % __version__,
+        ]
+        if isWrite:
+            cmd.append('--write')
+        if endtime:
+            cmd.extend(['--endtime', str('%f' % endtime)])
+        Popen(cmd)
+        LAST_ACTION = timestamp
+        if endtime and endtime > LAST_ACTION:
+            LAST_ACTION = endtime
+        LAST_FILE = targetFile
 
 
-def api(action, task, timestamp):
-    if task:
-        api_key = get_api_key()
-        if api_key:
-            cmd = ['python', API_CLIENT,
-                '--key', api_key,
-                '--instance', INSTANCE_ID,
-                '--action', action,
-                '--task', task,
-                '--time', str('%f' % timestamp)]
-            Popen(cmd)
+def away(now):
+    if LAST_ACTION == 0:
+        return False
+    duration = now - LAST_ACTION
+    if duration > AWAY_MINUTES * 60:
+        duration = int(duration)
+        units = 'seconds'
+        if duration > 60:
+            duration = int(duration / 60.0)
+            units = 'minutes'
+        if duration > 60:
+            duration = int(duration / 60.0)
+            units = 'hours'
+        return sublime\
+            .ok_cancel_dialog("You were away %d %s. Add time to current file?"\
+            % (duration, units), 'Yes, log this time')
+
+
+def enough_time_passed(now):
+    return (now - LAST_ACTION >= 5 * 60)
 
 
 class WakatimeListener(sublime_plugin.EventListener):
 
     def on_post_save(self, view):
-        api('write_file', view.file_name(), time.time())
+        api(view.file_name(), time.time(), isWrite=True)
 
     def on_activated(self, view):
-        api('open_file', view.file_name(), time.time())
+        now = time.time()
+        if enough_time_passed(now):
+            if away(now):
+                api(view.file_name(), LAST_ACTION, endtime=now)
+            else:
+                api(view.file_name(), now)
 
-    def on_deactivated(self, view):
-        api('close_file', view.file_name(), time.time())
+    def on_selection_modified(self, view):
+        now = time.time()
+        if enough_time_passed(now):
+            if away(now):
+                api(view.file_name(), LAST_ACTION, endtime=now)
+            else:
+                api(view.file_name(), now)
 
-
-if get_api_key() is None:
-    sublime.error_message('Missing your Wakati.Me api key')
