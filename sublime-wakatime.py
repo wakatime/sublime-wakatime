5095de4302e332df51607b25c5e30b53c4592ae4
Alan Hamlett <alan.hamlett@gmail.com>
2013-07-17 09:58:00 -0700

    Version 0.2.5

--- sublime-wakatime.py
+++ sublime-wakatime.py
@@ -5,7 +5,7 @@ Maintainer:  WakaTi.me <support@wakatime.com>
 Website:     https://www.wakati.me/
 ==========================================================="""
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 import time
 import uuid
