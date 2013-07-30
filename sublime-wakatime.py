a5c53f245fe8a09a6ea17fcb5053b58a0754780b
Alan Hamlett <alan.hamlett@gmail.com>
2013-07-29 23:11:46 -0700

    v0.2.10

--- sublime-wakatime.py
+++ sublime-wakatime.py
@@ -5,7 +5,7 @@ Maintainer:  WakaTi.me <support@wakatime.com>
 Website:     https://www.wakati.me/
 ==========================================================="""
 
-__version__ = '0.2.9'
+__version__ = '0.2.10'
 
 import sublime
 import sublime_plugin
