45662cc96a939fb6c76a32ed41d294ccd60aff15
Alan Hamlett <alan.hamlett@gmail.com>
2013-08-09 21:16:01 -0700

    v1.0.2. testing sublime package control versioning.

--- WakaTime.py
+++ WakaTime.py
@@ -5,7 +5,7 @@ Maintainer:  WakaTi.me <support@wakatime.com>
 Website:     https://www.wakati.me/
 ==========================================================="""
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 import sublime
 import sublime_plugin
