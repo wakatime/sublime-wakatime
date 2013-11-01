ca47c2308da9f5ba805be2535777d5e1ed186ae2
Alan Hamlett <alan.hamlett@prezi.com>
2013-10-31 17:20:12 -0700

    v1.4.10

--- WakaTime.py
+++ WakaTime.py
@@ -5,7 +5,7 @@ Maintainer:  WakaTi.me <support@wakatime.com>
 Website:     https://www.wakati.me/
 ==========================================================="""
 
-__version__ = '1.4.9'
+__version__ = '1.4.10'
 
 import sublime
 import sublime_plugin
