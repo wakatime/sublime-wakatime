d2a996e845ca7f7d3b4a8852530e964b4d7283d2
Alan Hamlett <alan.hamlett@gmail.com>
2014-07-25 01:01:39 -0700

    upgrade wakatime package to v2.0.4 to prevent logging namespace conflicts

--- packages/wakatime/wakatime/__init__.py
+++ packages/wakatime/wakatime/__init__.py
@@ -13,7 +13,7 @@
 from __future__ import print_function
 
 __title__ = 'wakatime'
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 __author__ = 'Alan Hamlett'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2014 Alan Hamlett'
@@ -52,7 +52,7 @@ except:
     from .packages import tzlocal3
 
 
-log = logging.getLogger(__name__)
+log = logging.getLogger('WakaTime')
 
 
 class FileAction(argparse.Action):
