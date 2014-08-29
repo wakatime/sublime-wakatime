253728545c560d7457afeb300d1a9f363566e51b
Alan Hamlett <alan.hamlett@gmail.com>
2014-08-29 12:50:55 -0700

    v2.0.10

--- WakaTime.py
+++ WakaTime.py
@@ -6,7 +6,7 @@ License:     BSD, see LICENSE for more details.
 Website:     https://wakatime.com/
 ==========================================================="""
 
-__version__ = '2.0.9'
+__version__ = '2.0.10'
 
 import sublime
 import sublime_plugin
