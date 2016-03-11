c64f125dc483ebdf44b584a0584fe43fb2ad4779
Alan Hamlett <alan.hamlett@gmail.com>
2016-03-11 09:23:08 -0800

    no need to check debug mode before logging

--- WakaTime.py
+++ WakaTime.py
@@ -256,11 +256,10 @@ def find_python_from_registry(location, reg=None):
                         sub_key=sub_key,
                     ))
     except WindowsError:
-        if SETTINGS.get('debug'):
-            log(DEBUG, 'Could not read registry value "{reg}\\{key}".'.format(
-                reg=reg,
-                key=location,
-            ))
+        log(DEBUG, 'Could not read registry value "{reg}\\{key}".'.format(
+            reg=reg,
+            key=location,
+        ))
 
     return val
 
